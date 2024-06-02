2ebcc1829eab0fa7086807b5e1ada0e3dce82cdb
manohar1780 <bhadu3217@gmail.com>
2024-06-02 19:07:33 +0530

    sdf

--- src/app/MnyMng.jsx
+++ src/app/MnyMng.jsx
@@ -9,6 +9,7 @@ const MnyMng = () => {
         profit_percent: 0,
         totalUsed: 0,
         totalLoss: 0,
+        total_amount: 0
     };
     const [formValue, setFormValue] = useState(initValue);
     const [calData, setCalData] = useState([]);
@@ -30,30 +31,46 @@ const MnyMng = () => {
     const handleSubmit = () => {
         setCalData([]);
         let amount = parseFloat(formValue.start_amount);
+        console.log(amount, 'smt')
         let percent = parseFloat(formValue.percent);
         let step = parseFloat(formValue.step);
         let profit_percent = parseFloat(formValue.profit_percent);
+        let total_amount = parseFloat(formValue.total_amount);
         let newData = [];
         let totalLoss = 0;
         let totalUsed = 0;
 
-        for (let i = 0;i < step;i++) {
+        for (let i = 0; i < step; i++) {
             totalLoss += amount;
             amount = ((profit_percent + 100) * totalLoss) / percent;
-            totalUsed += totalLoss;
-            newData.push({ step: i + 1, amount: amount, usedAmount: totalUsed });
+            console.log(amount, 'sd')
+            if (i == 0) {
+                totalUsed += amount + formValue.start_amount;
+            } else {
+                totalUsed += amount;
+            }
+            if (i == 0) {
+                total_amount = (total_amount - amount) - formValue.start_amount
+            } else {
+                total_amount = total_amount - amount
+            }
+            newData.push({ step: i + 1, amount: amount, usedAmount: totalUsed, total_amount: total_amount });
         }
         setCalData(newData);
     };
 
+    function showPositiveOrZero(number) {
+        return Math.max(number, 0);
+    }
+
     return (
-        <div className="flex items-stretch content-center justify-center h-screen bg-slate-800 text-slate-200">
-            <div className="p-4 my-3 font-mono pt-16 border border-slate-400 bg-cyan-950 rounded-bl-xl rounded-tl-xl">
+        <div className="flex gap-3 items-stretch content-center justify-center h-screen bg-[#0f172a] text-slate-200 p-3">
+            <div className="rounded-lg bg-[#1e293b] p-4 shadow-lg">
                 <div className="flex flex-col gap-3">
-                    <div className="w-full">
+                    <div className="w-[250px]">
                         <label
                             htmlFor="start_amount"
-                            className="block text-grey-darker text-md font-bold mb-1"
+                            className="block text-sm font-medium text-slate-300 pb-1"
                         >
                             Init Amount
                         </label>
@@ -61,7 +78,7 @@ const MnyMng = () => {
                             type="number"
                             name="start_amount"
                             placeholder="0"
-                            className="shadow appearance-none border bg-transparent border-red rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none"
+                            className="block w-full rounded-md border-slate-700 bg-[#334155] px-3 py-2 text-white placeholder-slate-400 outline-none"
                             value={formValue.start_amount}
                             onChange={(e) => handleChange(e)}
                         />
@@ -69,7 +86,7 @@ const MnyMng = () => {
                     <div className="w-full">
                         <label
                             htmlFor="percent"
-                            className="block text-grey-darker text-md font-bold mb-1"
+                            className="block text-sm font-medium text-slate-300 pb-1"
                         >
                             Return Percent
                         </label>
@@ -77,7 +94,7 @@ const MnyMng = () => {
                             type="number"
                             name="percent"
                             placeholder="0"
-                            className="shadow appearance-none border bg-transparent border-red rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none"
+                            className="block w-full rounded-md border-slate-700 bg-[#334155] px-3 py-2 text-white placeholder-slate-400 outline-none"
                             value={formValue.percent}
                             onChange={(e) => handleChange(e)}
                         />
@@ -85,7 +102,7 @@ const MnyMng = () => {
                     <div className="w-full">
                         <label
                             htmlFor="step"
-                            className="block text-grey-darker text-md font-bold mb-1"
+                            className="block text-sm font-medium text-slate-300 pb-1"
                         >
                             Step
                         </label>
@@ -93,7 +110,7 @@ const MnyMng = () => {
                             type="number"
                             name="step"
                             placeholder="0"
-                            className="shadow appearance-none border bg-transparent border-red rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none"
+                            className="block w-full rounded-md border-slate-700 bg-[#334155] px-3 py-2 text-white placeholder-slate-400 outline-none"
                             value={formValue.step}
                             onChange={(e) => handleChange(e)}
                         />
@@ -101,7 +118,7 @@ const MnyMng = () => {
                     <div className="w-full">
                         <label
                             htmlFor="profit_percent"
-                            className="block text-grey-darker text-md font-bold mb-1"
+                            className="block text-sm font-medium text-slate-300 pb-1"
                         >
                             Profit Percent
                         </label>
@@ -109,12 +126,28 @@ const MnyMng = () => {
                             type="number"
                             name="profit_percent"
                             placeholder="0"
-                            className="shadow appearance-none border bg-transparent border-red rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none"
+                            className="block w-full rounded-md border-slate-700 bg-[#334155] px-3 py-2 text-white placeholder-slate-400 outline-none"
                             value={formValue.profit_percent}
                             onChange={(e) => handleChange(e)}
                         />
                     </div>
-                    <button className="w-100 block bg-green-700 py-2 px-3 rounded-lg font-medium" onClick={handleSubmit}>
+                    <div className="w-full">
+                        <label
+                            htmlFor="total_amount"
+                            className="block text-sm font-medium text-slate-300 pb-1"
+                        >
+                            Total Amount
+                        </label>
+                        <input
+                            type="number"
+                            name="total_amount"
+                            placeholder="0"
+                            className="block w-full rounded-md border-slate-700 bg-[#334155] px-3 py-2 text-white placeholder-slate-400 outline-none"
+                            value={formValue.total_amount}
+                            onChange={(e) => handleChange(e)}
+                        />
+                    </div>
+                    <button className="w-100 block bg-green-700 py-2 mt-2 px-3 rounded-lg font-medium" onClick={handleSubmit}>
                         Save
                     </button>
                     <button className="w-100 block bg-slate-500 py-2 px-3 rounded-lg font-medium" onClick={handleClear}>
@@ -122,31 +155,39 @@ const MnyMng = () => {
                     </button>
                 </div>
             </div>
-            {
-                calData.length > 0 &&
-                <div className="p-3 my-3 font-mono border border-slate-400 bg-slate-800 rounded-br-xl rounded-tr-xl overflow-y-auto">
-                    <table class="min-w-full text-sm">
-                        <thead class="bg-cyan-950 text-xs uppercase font-medium border border-slate-400">
+            {calData.length > 0 &&
+                <div className="overflow-y-auto rounded-lg bg-[#1e293b] shadow-lg">
+                    <table class="min-w-full text-sm text-slate-300">
+                        <thead class="bg-[#334155] uppercase text-nowrap">
                             <tr>
-                                <th scope="col" class="px-6 py-3 text-left tracking-wider border border-slate-400">
+                                <th scope="col" class="p-4 w-[100px] text-start">
                                     Step
                                 </th>
-                                <th scope="col" class="px-6 py-3 text-left tracking-wider border border-slate-400">
+                                <th scope="col" class="p-4 w-[200px] text-start">
                                     next amount
                                 </th>
-                                <th scope="col" class="px-6 py-3 text-left tracking-wider">
+                                <th scope="col" class="p-4 w-[200px] text-start">
                                     require amount
+                                </th>
+                                <th scope="col" class="p-4 w-[200px] text-start">
+                                    Remaining amount
                                 </th>
                             </tr>
                         </thead>
-                        <tbody class="border border-slate-400 bg-cyan-900">
+                        <tbody>
+                            <tr className="border-b border-slate-700">
+                                <td className="p-4">1</td>
+                                <td className="p-4 whitespace-nowrap cursor-pointer">₹ {formValue.start_amount}</td>
+                                <td className="p-4">₹ {formValue.start_amount}</td>
+                                <td className="p-4">{showPositiveOrZero(formValue.total_amount - formValue.start_amount)}</td>
+                            </tr>
                             {calData.map((data, index) => (
-                                <tr className="border border-slate-400" key={index}>
-                                    <td class="pl-4 border border-slate-400 w-[200px]">
-                                        {index + 1}
+                                <tr className="border-b border-slate-700" key={index}>
+                                    <td class="p-4">
+                                        {index + 2}
                                     </td>
                                     <td
-                                        className="px-6 py-4 whitespace-nowrap cursor-pointer w-50 border border-slate-400 w-[200px]"
+                                        className="p-4 whitespace-nowrap cursor-pointer"
                                         title="click to copy"
                                         onClick={() => {
                                             navigator.clipboard.writeText((data.amount).toFixed(0));
@@ -160,8 +201,11 @@ const MnyMng = () => {
                                             {copiedIndex === index ? ' 📋copied!' : ''}
                                         </span>
                                     </td>
-                                    <td className="px-6 py-4 whitespace-nowrap w-50">
+                                    <td className="p-4 whitespace-nowrap">
                                         ₹ {(data.usedAmount).toFixed(0)}
+                                    </td>
+                                    <td className="p-4 whitespace-nowrap">
+                                        {showPositiveOrZero(data.total_amount)?.toFixed(0)}
                                     </td>
                                 </tr>
                             ))}
